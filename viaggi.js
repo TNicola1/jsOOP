28bdf6957a3f1a9e069d9c05e7e02b7062c97f34
TNicola1 <129531208+TNicola1@users.noreply.github.com>
2024-06-14 12:30:45 +0200

    Modifica index e print info

--- viaggi.js
+++ viaggi.js
@@ -10,23 +10,26 @@ class Vehicle {
     consumptionLtKm
     fuelType
     fuelPriceLt
+    incrementPC
     services = []
 
-    constructor(type, model, capacity, speedKmH, consumptionLtKm, fuelType, fuelPriceLt, services) {
+    constructor(type, model, capacity, speedKmH, consumptionLtKm, fuelType, fuelPriceLt, incrementPC, services) {
         this.type = type
         this.model = model
         this.capacity = capacity
+        this.incrementPC = incrementPC
         this.speedKmH = speedKmH
         this.consumptionLtKm = consumptionLtKm
         this.fuelType = fuelType
         this.fuelPriceLt = fuelPriceLt
-        this.services = services
+        this.services = [...services]
         this.reservations = 0
     }
 
     calcPassengerPrice(distance) {
         let basePrice = this.calcCompanyPrice(distance) / this.capacity
-        return basePrice * 1.5
+        let increment = basePrice * (this.incrementPC / 100)
+        return basePrice + increment
     }
 
     calcCompanyPrice(distance) {
@@ -83,23 +86,47 @@ class Vehicle {
 }
 
 class Airplane extends Vehicle {
-    constructor(type, model, capacity, speedKmH, consumptionLtKm, fuelType, fuelPriceLt, services)
+
+    defaultConsumptionLt
+
+    constructor(type, model, capacity, speedKmH, consumptionLtKm, fuelType, fuelPriceLt, services, defaultConsumptionLt)
     {
-        super(type, model, capacity, speedKmH, consumptionLtKm, fuelType, fuelPriceLt, services)
+        let incrementPC = 90
+        super(type, model, capacity, speedKmH, consumptionLtKm, fuelType, fuelPriceLt, incrementPC, services)
+        this.defaultConsumptionLt = defaultConsumptionLt
+    }
+
+    calcCompanyPrice(distance) {
+        let flightPrice = super.calcCompanyPrice(distance)
+        let defaultPrice = this.defaultConsumptionLt * this.fuelPriceLt
+        return flightPrice + defaultPrice
     }
 }
 
 class Train extends Vehicle {
     constructor(type, model, capacity, speedKmH, consumptionLtKm, fuelType, fuelPriceLt, services)
     {
-        super(type, model, capacity, speedKmH, consumptionLtKm, fuelType, fuelPriceLt, services)
+        let incrementPC = 60
+        super(type, model, capacity, speedKmH, consumptionLtKm, fuelType, fuelPriceLt, incrementPC, services)
     }
 }
 
 class Bus extends Vehicle {
-    constructor(type, model, capacity, speedKmH, priceKm, consumptionLtKm, fuelType, fuelPriceLt, services)
+    constructor(type, model, capacity, speedKmH, consumptionLtKm, fuelType, fuelPriceLt, services)
     {
-        super(type, model, capacity, speedKmH, priceKm, consumptionLtKm, fuelType, fuelPriceLt, services)
+        let incrementPC = 20
+        super(type, model, capacity, speedKmH, consumptionLtKm, fuelType, fuelPriceLt, incrementPC, services)
     }
 }
 
+const aereo = new Airplane("Aereo", "Boeing 747", 600, 988, 12, "cherosene", 1.5, ["Checkin online", "Pasti", "Wi-fi", "Tablet"], 4000)
+const treno = new Train("Treno", "Frecciarossa", 450, 360, 90, "diesel", 2.075, ["Cuccette", "Ristorante", "Aria condizionata", "Wi-fi"])
+const bus = new Bus("Bus", "Iveco", 50, 70, 20, "diesel", 2.075, ["Wi-fi", "Aria condizionata", "Sedili reclinabili"])
+
+// Stampa info mezzi
+let arrayMezzi = [aereo, treno, bus]
+
+for(const mezzo of arrayMezzi) {
+    mezzo.printInfo()
+    console.log(`Un viaggio in ${mezzo.type} per 100km costa ${mezzo.calcPassengerPrice(100)}`)
+}
