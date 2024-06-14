d5e741ccf07a1b2f05a3872b1ac03d39027f90c3
TNicola1 <129531208+TNicola1@users.noreply.github.com>
2024-06-14 16:38:05 +0200

    Modifiche esercizio audio e video

--- esercizio.js
+++ esercizio.js
@@ -19,27 +19,26 @@ class Book {
 
     borrow() {
         if(!this.#available) {
-            console.log("Il libro richiesto non è disponibile")
+            console.log(`Il libro "${this.#title}" non è disponibile, lo devi restituire!`)
         } else {
-            console.log("Il libro richiesto è disponibile")
+            console.log(`Il libro "${this.#title}" è disponibile`)
+            console.log(`Hai preso in prestito ${this.#title}. Ricordati di restituirlo!`)
             this.#available = false
         }
     }
 
     return() {
         if(!this.#available) {
-            console.log("Grazie per avere restituito il libro")
+            console.log(`Grazie per aver restituito "${this.#title}"`)
             this.#available = true
         } else {
-            console.log("Il libro non è stato dato in prestito")
+            console.log(`"${this.#title}" non è stato ancora prestato`)
         }
     }
 
     showInfo() {
         console.log(`Il titolo del libro è "${this.#title}"`)
         console.log(`L'autore del libro è ${this.#author}`)
-        console.log(`Il libro ${this.#available ? "è" : "non è"} disponibile`)
-
     }
 }
 
@@ -54,13 +53,14 @@ class Novel extends Book {
     showInfo() {
         super.showInfo()
         console.log(`Il genere del libro è ${this.#genre}`)
+        console.log(`Il libro ${this.available ? "è" : "non è"} disponibile`)
     }
 }
 
 class Assay extends Book {
     #topic
 
-    constructor(title, author, title) {
+    constructor(title, author, topic) {
         super(title, author)
         this.#topic = topic
     }
@@ -68,7 +68,7 @@ class Assay extends Book {
     showInfo() {
         super.showInfo()
         console.log(`L'argomento del libro è ${this.#topic}`)
-        
+        console.log(`Il libro ${this.available ? "è" : "non è"} disponibile`)
     }
 }
 
@@ -85,7 +85,74 @@ class Manual extends Book {
 
     showInfo() {
         super.showInfo()
-        console.log(`La categori del libro è ${this.#category}`)
+        console.log(`La categoria del libro è ${this.#category}`)
         console.log(`E' la ${this.#edition} edizione`)
+        console.log(`Il libro ${this.available ? "è" : "non è"} disponibile`)
     }
 }
+
+class Video extends Book {
+    #framerate
+    #download
+
+    constructor(title, author, framerate) {
+        super(title, author)
+        this.#framerate = framerate
+        this.#download = 0
+    }
+
+    borrow() {
+        console.log("Prestito non disponibile")
+    }
+
+    return() {
+        console.log("Restituzione non disponibile")
+    }
+
+    download() {
+        this.#download += 1
+        console.log("Il video è stato scaricato")
+    }
+
+    showInfo() {
+        super.showInfo()
+        console.log(`Il video ha un framerate ${this.#framerate}`)
+    }
+}
+
+class Audio extends Book {
+    #format
+    #download
+
+    constructor(title, author, format) {
+        super(title, author)
+        this.#format = format
+        this.#download = 0
+    }
+
+    borrow() {
+        console.log("Prestito non disponibile")
+    }
+
+    return() {
+        console.log("Restituzione non disponibile")
+    }
+
+    download() {
+        this.#download += 1
+        console.log("L'audio è stato scaricato")
+    }
+
+    showInfo() {
+        super.showInfo()
+        console.log(`L'audio è in formato ${this.#format}`)
+    }
+}
+
+const romanzo = new Novel("La freccia nera", "Robert Louis Stevenson", "Storico")
+romanzo.borrow()
+romanzo.showInfo()
+romanzo.borrow()
+romanzo.showInfo()
+romanzo.return()
+romanzo.showInfo()
